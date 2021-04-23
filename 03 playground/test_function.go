45c9ec7f29de3704d4d33c68bb110edbe9f5da9c
bujosa <davidbujosa@gmail.com>
2021-04-23 02:33:05 -0400

    Added anotations

--- 03 playground/test_function.go
+++ 03 playground/test_function.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
+// global variable
+var z = 100
+
+// you can identifier w is of TYPE int
+var w int
+
 func main() {
 	fmt.Println("Am testing a foo function")
+
+	// call a foo function
 	foo()
 
+	// simple loop
 	for i := 0; i < 100; i++ {
-		if i %2 == 0 {
+		if i % 50 == 0 {
 			fmt.Println(i)
 		}
 	}
+
+	// Short declaration operator
+	// Declare a variable and assing a value
+	x := 42
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + 24 
+	fmt.Println(y)
 }
 
+// this is foo function
 func foo(){
-	fmt.Println("Running foo function")
+	fmt.Println("Running foo function: ", z)
 }
