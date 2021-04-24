360a1eae81158a69dea0832d569f035e60191318
bujosa <davidbujosa@gmail.com>
2021-04-24 03:09:51 -0400

    Added bool types

--- 04 types/main.go
+++ 04 types/main.go
@@ -8,9 +8,15 @@ var a int = 42
 type bujosa int
 var b bujosa
 
+var x bool 
+
+// Use own type
 func main() {
 	b = 1020
 	fmt.Println(b)
 	fmt.Printf("%T\n", b)
 	a = int(b)
+	fmt.Println(x)
+	x = true
+	fmt.Println(x)
 }
