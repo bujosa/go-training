e397910443e8b74bb2c54227df2bcd10d785b420
bujosa <davidbujosa@gmail.com>
2021-04-24 03:16:30 -0400

    Added string type, uint8 type

--- 04 types/main.go
+++ 04 types/main.go
@@ -9,14 +9,33 @@ type bujosa int
 var b bujosa
 
 var x bool 
+var y float64
 
 // Use own type
 func main() {
 	b = 1020
+	y = 42.334324
 	fmt.Println(b)
 	fmt.Printf("%T\n", b)
 	a = int(b)
+
+	// boolean
 	fmt.Println(x)
 	x = true
 	fmt.Println(x)
+
+	// float64
+	fmt.Println(y)
+
+	// string
+	s := "Hello Bujosa"
+	fmt.Printf("%T\n", s)
+
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n",bs)
+
+	for i, v := range s {
+		fmt.Println(i, v)
+	}
 }
