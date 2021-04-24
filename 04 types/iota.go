b54bc413a0a8cd476f2cbb5f16b7e66c6e4b9833
bujosa <davidbujosa@gmail.com>
2021-04-24 03:32:24 -0400

    Added other example with iota

--- 04 types/iota.go
+++ 04 types/iota.go
@@ -11,8 +11,21 @@ const (
 	c
 )
 
+const (
+	_ = iota
+	kb = 1 << (iota * 10)
+	mb = 1 << (iota * 10)
+	gb = 1 << (iota * 10)
+)
+
 func main() {
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
+
+	fmt.Printf("%d\t\t\t%b\n", kb, kb)
+	fmt.Printf("%d\t\t\t%b\n", mb, mb)
+	fmt.Printf("%d\t\t%b\n", gb, gb)
 }
+
+// example wit ioda
