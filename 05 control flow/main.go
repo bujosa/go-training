0857b283a19a47037c16d4b2c9849655adac2a02
bujosa <davidbujosa@gmail.com>
2021-04-29 03:54:58 -0400

    Added more loops

--- 05 control flow/main.go
+++ 05 control flow/main.go
@@ -8,4 +8,19 @@ func main() {
 	for i := 0; i <= 100; i++{
 		fmt.Println(i)
 	}
+
+	// one condintion
+	x := 100
+	for x > 0 {
+		fmt.Println(x)
+		x -= 2
+	}
+
+	for {
+		if x > 100 {
+			break
+		}
+		fmt.Println(x)
+		x++
+	}
 }
