3f71e1581d8bbd5c84b4fee3bf2f3b2f5d4ae6a2
bujosa <davidbujosa@gmail.com>
2021-05-01 03:14:39 -0400

    Added more functions

--- 08 functions/main.go
+++ 08 functions/main.go
@@ -6,8 +6,24 @@ import (
 
 func main(){
 	foo()
+	bar("Bujosa")
+	x, y := compare("Bujosa", "Faiella")
+	fmt.Println(x, y)
 }
 
 func foo() {
 	fmt.Println("Hello I AM B U J O S A")
+}
+
+// Everything in Go is "PASS BY VALUE"
+func bar(s string){
+	fmt.Println(s)
+}
+
+func compare(L string, R string) (string, bool) {
+	if L == R {
+		return "Is Equal", true
+	}
+
+	return "Is not Equal", false
 }
