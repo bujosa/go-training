a619923cb60b660c83e408ad86c37e1054c59d24
bujosa <davidbujosa@gmail.com>
2021-05-01 18:17:09 -0400

    Fixed interfaces

--- 08 functions/interfaces.go
+++ 08 functions/interfaces.go
@@ -8,6 +8,11 @@ type person struct {
 	first string
 }
 
+type secretAgent struct {
+	person
+	ltk bool
+}
+
 func (s person) speak(){
 	fmt.Println("I am", s.first)
 }
@@ -16,6 +21,31 @@ type human interface {
 	speak()
 }
 
+func bar(h human) {
+	switch  h.(type) {
+	case person:
+      fmt.Println("I was passed into bar", h.(person).first)
+	case secretAgent:
+      fmt.Println("I was passed into bar", h.(secretAgent).first)
+	default:
+	  fmt.Println("Unknown")		
+	}	
+}
+
 func main() {
-	fmt.Println("Hello world")
+	p := person{
+		first: "David",
+	}
+
+	s := secretAgent{
+		person: p,
+		ltk: true,
+	}
+
+	// Polymorphims
+	s.speak()
+	p.speak()
+
+	bar(s)
+	bar(p)
 }
