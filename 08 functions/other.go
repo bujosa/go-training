ab1a726f0da9dd6b3ff0284534d197d5d6d63ef9
bujosa <davidbujosa@gmail.com>
2021-05-01 03:33:48 -0400

    added defer definition

--- 08 functions/other.go
+++ 08 functions/other.go
@@ -5,9 +5,15 @@ import (
 )
 
 func main(){
-	foo(1, 2, 3, 4)
+	// defer is uses to close files, process, etc ...
+	defer foo(1, 2, 3, 4)
+	bar()
 }
 
 func foo(x ...int) {
-	fmt.Println(x)
+	fmt.Println(x, "Second")
+}
+
+func bar() {
+	fmt.Println("First")
 }
