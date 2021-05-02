7d823fa5740cbf1c7e1bdbe5c0abf6aa2d5e78fa
bujosa <davidbujosa@gmail.com>
2021-05-01 20:25:40 -0400

    Added marchal

---
+++ 10 application/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type person struct {
+	First string
+	Last  string
+	Age   int
+}
+
+func main() {
+	p1 := person{
+		First: "David",
+		Last:  "Bujosa",
+		Age:   21,
+	}
+	fmt.Println(p1)
+
+	// uses marshal from json package
+	bs, err := json.Marshal(p1)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(bs))
+}
