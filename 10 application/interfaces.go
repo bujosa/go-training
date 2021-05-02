29bf57607b29636aad61a816d570a791604470e0
bujosa <davidbujosa@gmail.com>
2021-05-01 21:35:34 -0400

    added sort

--- 10 application/interfaces.go
+++ 10 application/interfaces.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println("Hello world")
 	fmt.Fprintln(os.Stdout, "Hello world")
+	io.WriteString(os.Stdout, "Hello world")
 }
