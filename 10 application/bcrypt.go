22f92802d003a268416bf83d64a3cfcd2a74eb95
bujosa <davidbujosa@gmail.com>
2021-05-01 22:00:26 -0400

    finished bcrypt

--- 10 application/bcrypt.go
+++ 10 application/bcrypt.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+func main() {
+	s := "passwordTest"
+	bs, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.MinCost)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(s)
+	fmt.Println(bs)
+
+	err = bcrypt.CompareHashAndPassword(bs, []byte(s))
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("You're logged in bujosa code")
 }
