59ba7406f8145be85ae190f417b53b8562c047ce
bujosa <davidbujosa@gmail.com>
2021-05-02 02:18:17 -0400

    Added wait group

--- 11 concurrency/main.go
+++ 11 concurrency/main.go
@@ -1,15 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+// uses wait group
+var wg sync.WaitGroup
+
 // concurrency
 func main() {
-	foo()
+	fmt.Println("ARCH", runtime.GOARCH)
+	fmt.Println("OS", runtime.GOOS)
+	fmt.Println("CPUs", runtime.NumCPU())
+	fmt.Println("Goroutines", runtime.NumGoroutine())
+	wg.Add(1)
+	go foo()
 	bar()
+
+	wg.Wait()
 }
 
 func foo() {
-
+   for i := 0; i < 10; i++ {
+	   fmt.Println("foo:",i)
+   }
+   wg.Done()
 }
 
 func bar() {
-
+   for i := 0; i < 10; i++ {
+	   fmt.Println("bar:",i)
+   }
 }
