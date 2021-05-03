9d35fac1b6f50a642ae6c756a82a8e7353d65236
bujosa <davidbujosa@gmail.com>
2021-05-03 00:58:49 -0400

    finished race condiction

--- 11 concurrency/race-condiction.go
+++ 11 concurrency/race-condiction.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
 
+
+func main() {
+	fmt.Println("CPUs", runtime.NumCPU())
+	fmt.Println("Goroutine", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			//time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutine:", runtime.NumGoroutine())
+	}
+
+	wg.Wait()
+	fmt.Println("Goroutine:", runtime.NumGoroutine())
+	fmt.Println("Counter:", counter)
 }
