330c71e699bde108855ab15d3c04ad9574f4efe3
bujosa <davidbujosa@gmail.com>
2021-05-03 01:05:58 -0400

    added atomic

--- 11 concurrency/atomic.go
+++ 11 concurrency/atomic.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"sync"
+	"sync/atomic"
 )
 
 
@@ -11,25 +12,17 @@ func main() {
 	fmt.Println("CPUs", runtime.NumCPU())
 	fmt.Println("Goroutine", runtime.NumGoroutine())
 
-	counter := 0
+	var counter int64
 
 	const gs = 100
-
 	var wg sync.WaitGroup
 	wg.Add(gs)
 
-	// uses mutex
-	var mu sync.Mutex
-
 	for i := 0; i < gs; i++ {
 		go func() {
-			mu.Lock()
-			v := counter
-			//time.Sleep(time.Second)
+			atomic.AddInt64(&counter,1)
+			fmt.Println(counter)
 			runtime.Gosched()
-			v++
-			counter = v
-			mu.Unlock()
 			wg.Done()
 		}()
 		fmt.Println("Goroutine:", runtime.NumGoroutine())
