fe556a70b729ecef07eb4581a7d2ac6fb5b4f9dd
bujosa <davidbujosa@gmail.com>
2021-05-09 20:46:59 -0400

    finished test

--- 12 channels/functions.go
+++ 12 channels/functions.go
@@ -1,12 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
 	c := make(chan int)
 	// send
+	go foo(c)
+
+	//receive
+	go bar(c)
+
+	fmt.Println("about to exit")
 }
 
 //send
-func foo() {}
+func foo(c chan<- int) {
+	c <- 42
+}
 
 // receive
-func bar() {}
+func bar(c <-chan int) {
+	fmt.Println(<-c)
+}
