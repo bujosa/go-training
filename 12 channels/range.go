ff7b89c14dc9e4b951e785703ed3eff246af6605
bujosa <davidbujosa@gmail.com>
2021-05-15 15:51:59 -0400

    Added select

--- 12 channels/range.go
+++ 12 channels/range.go
@@ -6,7 +6,12 @@ func main() {
 	c := make(chan int)
 	
 	// send
-	go foo(c)
+	go func(){
+		for i:= 0; i<100; i++ {
+				c <- i
+		}
+		close(c)
+	}()
 
 	//receive
 	for v := range c {
@@ -16,10 +21,3 @@ func main() {
 	fmt.Println("about to exit")
 }
 
-//send
-func foo(c chan<- int) {
-	for i:= 0; i<100; i++ {
-		c <-i
-	}
-	close(c)
-}
