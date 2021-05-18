68168b3f08530e94f8dc05efd75e19b3edb096ad
bujosa <davidbujosa@gmail.com>
2021-05-18 19:41:06 -0400

    Added log

--- 13 error handling/main.go
+++ 13 error handling/main.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
 
 func main() {
 	n, err := fmt.Println("Hello")
 
 	if err != nil {
        fmt.Println(err)
+	}
+
+	// file
+
+	_, err = os.Open("no-file.txt")
+
+	if err != nil{
+	   log.Println("err happened", err)
 	}
 
 	fmt.Println(n)
