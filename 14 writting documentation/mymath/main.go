c6305646941a5eea1a05d37e81344dd9ed450b4f
bujosa <davidbujosa@gmail.com>
2021-05-22 21:34:21 -0400

    added documentation

--- 14 writting documentation/mymath/main.go
+++ 14 writting documentation/mymath/main.go
@@ -1,5 +1,7 @@
+// Package mymath provides ACME inc math solutions
 package mymath
 
+// Sum adds an unlimited number of values of type int.
 func Sum(xi ...int) int {
 	sum := 0
 	for _, v := range xi {
