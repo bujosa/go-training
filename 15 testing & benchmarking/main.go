d827bb3f6fa84c2fa562f23080d7185808b94462
bujosa <davidbujosa@gmail.com>
2021-05-24 23:20:46 -0400

    Added sum

---
+++ 15 testing & benchmarking/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("2 + 3=",mySum(2, 3))
+	fmt.Println("10 + 5=",mySum(10, 5))
+	fmt.Println("7 + 7=",mySum(7, 7))
+}
+
+func mySum(xi ...int) int {
+	sum := 0
+
+	for _, v := range xi {
+		sum += v
+	}
+
+	return sum
+}
