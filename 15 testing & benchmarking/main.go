47d29af8c431eb2a52efe3b7497649dad270a6f5
bujosa <davidbujosa@gmail.com>
2021-05-24 23:22:27 -0400

    Fixed main

--- 15 testing & benchmarking/main.go
+++ 15 testing & benchmarking/main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("2 + 3=",mySum(2, 3))
-	fmt.Println("10 + 5=",mySum(10, 5))
-	fmt.Println("7 + 7=",mySum(7, 7))
+	fmt.Println("2 + 3 =",mySum(2, 3))
+	fmt.Println("10 + 5 =",mySum(10, 5))
+	fmt.Println("7 + 7 =",mySum(7, 7))
 }
 
 func mySum(xi ...int) int {
