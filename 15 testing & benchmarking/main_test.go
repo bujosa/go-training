b8b4303d96f661c5b7883c04b3b422604b51660e
bujosa <davidbujosa@gmail.com>
2021-05-27 22:15:37 -0400

    Finished test

--- 15 testing & benchmarking/main_test.go
+++ 15 testing & benchmarking/main_test.go
@@ -2,8 +2,9 @@ package main
 
 import "testing"
 
-func TestMySum(t *testing) {
-	if mySum(2, 3) != 5{
-		t.Error("")
+func TestMySum(t *testing.T) {
+	x := mySum(2, 3)
+	if  x != 5 {
+		t.Error("Expected", 5, "Got", x)
 	}
 }
