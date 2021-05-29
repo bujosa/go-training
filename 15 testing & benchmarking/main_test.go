da58dcbd7162268f6df6bcf65605621e2e7aa068
bujosa <davidbujosa@gmail.com>
2021-05-28 21:06:51 -0400

    Fixed main test

--- 15 testing & benchmarking/main_test.go
+++ 15 testing & benchmarking/main_test.go
@@ -3,8 +3,22 @@ package main
 import "testing"
 
 func TestMySum(t *testing.T) {
-	x := mySum(2, 3)
-	if  x != 5 {
-		t.Error("Expected", 5, "Got", x)
+
+	type test struct {
+		data []int
+		answer int
+	}
+
+	tests := []test{
+		test{[]int{21, 21}, 42},
+		test{[]int{10, 11}, 21},
+		test{[]int{2, 3}, 5},
+	}
+
+	for _, v:= range tests {
+		x := mySum(v.data...)
+		if  x != v.answer {
+			t.Error("Expected", v.answer, "Got", x)
+		}
 	}
 }
